1eabd0aa73d0998083bddb612af72f0300fd4f11
Nisha <nishavijai@gmail.com>
2026-01-07 14:16:05 +0100

    fix redirect url

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 
-// Page component
+// Page components
 import LandingPage from "../pages/LandingPage";
 import { SearchPage } from "../pages/SearchPage";
 import LayoutPage from "../pages/LayoutPage";
@@ -10,8 +10,12 @@ export default function Routers() {
     <BrowserRouter>
       <Routes>
         <Route element={<LayoutPage />}>
+          {/* Home page */}
           <Route index path="/" element={<LandingPage />} />
+
+          {/* Search page accessible from both URLs */}
           <Route path="/searchpage" element={<SearchPage />} />
+          <Route path="/search" element={<SearchPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
